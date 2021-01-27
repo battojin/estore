910e648dff6fdf79fa6c23025c20cb134fec8a5c
sogekihei <seitensogekihei@protonmail.ch>
2021-01-27 16:43:54 +0300

    fixed routing

--- client/config/root.js
+++ client/config/root.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
-import { Switch, Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import store, { history } from '../redux'
 import Home from '../components/home'
 import Basket from '../components/basket'
@@ -12,7 +12,7 @@ const Root = () => {
       <ConnectedRouter history={history}>
         <Switch>
           <Route exact path="/" component={() => <Home />} />
-          <Route exact path="/basket" components={() => <Basket />} />
+          <Route exact path="/basket" component={() => <Basket />} />
         </Switch>
       </ConnectedRouter>
     </Provider>
