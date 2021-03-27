3aff5ab529f026683e28db5a5d7e065a86baeada
sogekihei <seitensogekihei@protonmail.ch>
2021-03-27 16:06:34 +0300

    css updated

--- client/components/card.js
+++ client/components/card.js
@@ -8,13 +8,13 @@ const Card = ({ item, currencyValue, basketGood }) => {
   return (
     <div className="card p-1">
       <img alt="item" src={item.image} />
-      <div className="card-body">
+      <div className="card-body pl-2">
         <div>Price: {(item.price * currencyValue).toFixed(2)}</div>
         <p>{item.title}</p>
         <p className="mb-1">In Basket: {basketGood}</p>
         <button
           type="button"
-          className="btn btn-light"
+          className="btn btn-light mt-2"
           onClick={() => {
             dispatch(getItem(item.id))
             dispatch(getHeaderData())
